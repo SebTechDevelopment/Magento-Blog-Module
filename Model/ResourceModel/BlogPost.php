0280075f9f7a98e3448af740e54517f3e19c1af3
sebalang1 <88249783+sebalang1@users.noreply.github.com>
2022-01-01 15:38:06 +0100

    Updated ResourceModel of blogpost

--- Model/ResourceModel/BlogPost.php
+++ Model/ResourceModel/BlogPost.php
@@ -6,6 +6,9 @@ use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
 class BlogPost extends AbstractDb
 {
+    /**
+     * @return void
+     */
     protected function _construct()
     {
         $this->_init('sebtech_blog_post', 'id');
--- Model/ResourceModel/BlogPost/Collection.php
+++ Model/ResourceModel/BlogPost/Collection.php
@@ -6,8 +6,12 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use SebTech\Blog\Model\BlogPost as BlogPostModel;
 use SebTech\Blog\Model\ResourceModel\BlogPost as BlogPostResourceModel;
 
+
 class Collection extends AbstractCollection
 {
+    /**
+     * @return void
+     */
     protected function _construct()
     {
         $this->_init(BlogPostModel::class, BlogPostResourceModel::class);
